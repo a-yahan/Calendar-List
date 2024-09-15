0443515877fa0688d49dd0e5df9b9b443cf8f14f
Yahan Yang <ayahan@unc.edu>
2024-09-15 03:27:15 -0400

    new progress

--- calList.js
+++ calList.js
@@ -1,6 +1,7 @@
 //This is my Calendar List Project.
 
 const List = new Array();
+let output = "";
 
 function createTask()
 {
@@ -8,7 +9,7 @@ function createTask()
     let dueDate = document.getElementById("dueDate").value;
     let priority = document.getElementById("priority").value;
     let notes = document.getElementById("notes").value;
-    addTask(name, dueDate, priority, notes)
+    addTask(name, dueDate, priority, notes);
 }
 
 function addTask(name,dueDate,priority,notes)
@@ -23,21 +24,23 @@ function addTask(name,dueDate,priority,notes)
     
         Complete()
         {
-            this.taskStatus = "Completed"
+            this.taskStatus = "Completed";
         }
 
     };
     List.push(task);
 }
 
-function printAll()
-{
-    for (let i = 0; len = List.length; i < len)
-    {
-        document.getElementById("Task-List").innerHTML =
-        List[i].taskName + ", " + List[i].taskDueDate + ", " + List[i].taskPriority + ", " + List[i].taskNotes + ", " + List[i].taskStatus;
-    }
-}
+function printTask()
+        {
+            for (let i=0, len = List.length; i < len;)
+            {
+                output = output + "<br>" + List[i].taskName;
+                i++; 
+            }
+            document.getElementById("Task-List").innerHTML = output;
+            
+        }
 
 
 
