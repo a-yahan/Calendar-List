794ef9ff671c3a8b1efc369be147576479d3b1b2
Yahan Yang <ayahan@unc.edu>
2024-09-15 03:00:27 -0400

    New changes

--- calList.js
+++ calList.js
@@ -1,38 +1,41 @@
 //This is my Calendar List Project.
 
-
-class Tasks
-{
-    constructor(name,dueDate,priority,notes)
-    {
-        this.name = name;
-        this.dueDate = dueDate;
-        this.priority = priority;
-        this.notes = notes;
-        this.completion = false;
-    }
-}
-
-
 const List = new Array();
 
-
-function addTask()
+function createTask()
 {
-    let taskName = document.getElementById("name");
-    let dueDate = document.getElementById("dueDate");
-    let prioprity = document.getElementById("priority");
-    let notes = document.getElementById("notes");
-    List.push(new Task(taskName, dueDate, prioprity, notes));
+    let name = document.getElementById("name").value;
+    let dueDate = document.getElementById("dueDate").value;
+    let priority = document.getElementById("priority").value;
+    let notes = document.getElementById("notes").value;
+    addTask(name, dueDate, priority, notes)
 }
 
+function addTask(name,dueDate,priority,notes)
+{
+    const task =
+    {
+        taskName: name,
+        taskDueDate: dueDate,
+        taskPriority: priority,
+        taskNotes: notes,
+        taskStatus: "In-Progress",
+    
+        Complete()
+        {
+            this.taskStatus = "Completed"
+        }
+
+    };
+    List.push(task);
+}
 
 function printAll()
 {
     for (let i = 0; len = List.length; i < len)
     {
         document.getElementById("Task-List").innerHTML =
-        List[i].name + ", " + List[i].dueDate + ", " + List[i].prioprity + ", " + List[i].notes + ", " + List[i].completion;
+        List[i].taskName + ", " + List[i].taskDueDate + ", " + List[i].taskPriority + ", " + List[i].taskNotes + ", " + List[i].taskStatus;
     }
 }
 
@@ -41,7 +44,7 @@ function printAll()
 
 
 
-document.getElementById("createTask").addEventListener("click", addTask);
-document.getElementById("printTask").addEventListener("click", printAll);
+//document.getElementById("createTask").addEventListener("click", addTask);
+//document.getElementById("printTask").addEventListener("click", printAll);
 
 
